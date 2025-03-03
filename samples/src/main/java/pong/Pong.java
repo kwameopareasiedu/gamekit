3760d8a80847c00fd1e723c1b736949e7362280a
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2025-03-03 21:47:21 +0000

    Moved parent module sources to engine module. Added samples module for testing during development

---
+++ samples/src/main/java/pong/Pong.java
@@ -0,0 +1,10 @@
+package pong;
+
+import dev.gamekit.Game;
+
+public class Pong extends Game {
+  public static void main(String[] args) throws InterruptedException {
+    Pong game = new Pong();
+    game.run();
+  }
+}
