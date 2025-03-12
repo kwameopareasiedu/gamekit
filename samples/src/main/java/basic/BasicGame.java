d18f161e328286a0cfeeb5a2d595f725c1e7eac1
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2025-03-12 22:55:03 +0000

    Updated BasicGame sample with Renderer calls

--- samples/src/main/java/basic/BasicGame.java
+++ samples/src/main/java/basic/BasicGame.java
@@ -2,11 +2,14 @@ package basic;
 
 import dev.gamekit.core.Application;
 import dev.gamekit.core.Input;
+import dev.gamekit.core.Renderer;
 import dev.gamekit.scene.Scene;
 
 import java.awt.*;
 
 public class BasicGame extends Scene {
+  private boolean isPressed = false;
+
   public BasicGame() {
     super("Basic Game");
   }
@@ -21,13 +24,21 @@ public class BasicGame extends Scene {
   }
 
   @Override
-  public void onRender(Graphics2D g) {
+  public void onUpdate() {
+    super.onUpdate();
+
+    isPressed = Input.isKeyPressed(Input.KEY_SPACE);
+  }
+
+  @Override
+  public void onRender() {
+    super.onRender();
     // Clear the screen with black
-    g.setColor(Color.BLACK);
-    g.fillRect(0, 0, 800, 600);
+    Renderer.setColor(Color.BLACK);
+    Renderer.clear(0, 0, 800, 600);
 
     // Draw a red or blue square based on if the space bar is pressed
-    g.setColor(Input.isKeyPressed(Input.KEY_SPACE) ? Color.RED : Color.BLUE);
-    g.fillRect(300, 200, 200, 200);
+    Renderer.setColor(isPressed ? Color.RED : Color.BLUE);
+    Renderer.rect(400, 300, 200, 200, true);
   }
 }
